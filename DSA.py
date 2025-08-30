da4cf9cdc4926d70f9e442a70f6e30ece66db0bc
scorpix-com <bharath.kumar.aids@gmail.com>
2025-08-30 22:27:34 +0530

    Bubble Sort

--- DSA.py
+++ DSA.py
@@ -1,41 +1,20 @@
-# ⚠️ Two pointer
 
-list = [int(input()) for i in range(5)]
-target = int(input("Enter target value (a + b = )"))
-l = 0
-r = len(list) - 1
+# ⚠️ Bubble Sort
 
-while l < r :
-    if list[l] + list[r] == target:
-        print(l, r)
+arr = [1,5,7,3,2]
+
+n = len(arr)
+swap = False
+
+for i in range(n -1):
+    for j in range(n -1):
+        if arr[j] > arr[j + 1]:
+            arr[j + 1], arr[j] = arr[j], arr[j + 1]
+            swap = True
+    print(arr)
+    if swap == False:
         break
-    elif list[l] + list[r] < target:
-        l += 1 
-    elif list[l] + list[r] > target:
-        r -= 1 
-    
-else:
-    print("No combination found..")
+    else:
+        swap = False
 
-
-'''for _ in range(len(list)): #Using For loop
-    if list[l] + list[r] == target:
-        print(l, r)
-        break
-    elif list[l] + list[r] < target: 
-        l += 1'''
-    
-
-
-# ⚠️ Two pointer palindrome
-
-def palindrome(s):
-    l = 0
-    r = len(s) - 1
-    while l < r :
-        if s[l] != s[r]:
-            return False
-        l += 1
-        r -= 1
-    return True
-print(palindrome('abaca'))
+print(f"Result: {arr}")
