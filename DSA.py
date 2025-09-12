0029d04618a3c973041f9de442cb186e685b3f37
scorpix-com <bharath.kumar.aids@gmail.com>
2025-09-12 20:06:48 +0530

    Linked List

--- DSA.py
+++ DSA.py
@@ -1,61 +1,84 @@
-# ⚠️ Queue
+# ⚠️ Linked List
 
-queue = []
+'''Created Linked list with basics of class and objects'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-# Enqueue
+Head = Node(10)
+Node1 = Node(20)
+Node2 = Node("Hi")
 
-queue.append(1)
-queue.append(2.0)
-queue.append("Hello")
-queue.append('A')
+Head.next = Node1
+Node1.next = Node2
 
-print(queue)
+curr = Head
+while curr is not None:
+    print(curr.data)
+    curr = curr.next
 
-# Dequeue
+print(Head.data, Head.next)
+# ---------------------------------------------------------
 
-popped1 = queue.pop(0)
-popped2 = queue.pop(0)
+'''This is main part of Linked List'''
 
-print(popped1,'\n',popped2)
 
-# Front
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def add(self, data) :
+        newNode = Node(data)
+        if self.head is None:
+            self.head = newNode
+        else:
+            cur = self.head
+            while (cur.next is not None):
+                cur = cur.next
+            cur.next = newNode
+    def display(self):
+        if (self.head is not None):
+            cur = self.head
+            while (cur is not None):
+                print(cur.data, end= " ")
+                cur = cur.next
+            print()
+        else: print("Node is empty.")
+    
+    def remove(self, target):
+        if self.head is None: print("It has no values in it.")
+        else:
+            if self.head.data == target:
+                self.head = self.head.next
+            else:
+                cur = self.head
 
-print(queue[0])
+                while cur.next is not None and cur.next.data != target:
+                    cur = cur.next
+                if cur.next is not None:
+                    cur.next = cur.next.next
+    def search(self, target):
+        if self.head is not None:
+            cur = self.head
+            if self.head.data == target:
+                print("Found")
+                return 0
+            while (cur is not None):
+                if cur.data == target:
+                    print("Found")
+                    return None
+                cur = cur.next
+            else: print("Not found")
+        else: print("Linked list is empty.")
 
-# Empty
 
-print(len(queue) == 0)
-
-''' Using Queue Library'''
-
-from queue import Queue
-
-q = Queue()
-
-q.put(5)
-q.put(2.0)
-q.put("Hello")
-
-print(q)
-
-# Dequeue
-
-popped = q.get()
-popped1 = q.get()
-
-print(popped, popped1)
-
-# Front
-
-print(q.queue[0])
-
-# Empty
-print(q.empty())
-
-# Full
-
-print(q.full())
-
-'''We can also use
-    ---from collections import dequeue
-    to use queue operations, store and access them.'''
+list = LinkedList()
+list.add(10)           
+list.add(15)           
+list.add("Hi")     
+list.add(11)     
+list.display()      
+list.remove(15)
+list.remove("Hi")
+list.display()
+list.search(15)
